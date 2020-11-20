de7b7662b00c8afeff95594597b9c19fc7b2ea87
yangm90 <mingchen.yang316@gmail.com>
2020-11-19 23:42:23 -0500

    2020-11-19 Update3 Create Video2Image.py. Transform videos to images.

---
+++ Video2Image.py
@@ -0,0 +1,30 @@
+# CUHK dataset, transform videos to images
+import cv2
+import glob
+
+num = 1
+
+Video_dir = 'S:/CUHK/Train/training_videos/'
+Image_dir = 'S:/CUHK/Train/training/'
+
+Video_path = glob.glob(Video_dir + '/*.avi')
+
+for path in Video_path:
+    cap = cv2.VideoCapture(path)
+
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps_num = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+    print(fps, width, height, fps_num)
+
+    success, image = cap.read()
+
+    while success:
+        Image_path = Image_dir + str(num).zfill(5) + '.png'
+        cv2.imwrite(Image_path, image)
+        print(Image_path)
+        num += 1
+        success, image = cap.read()
+
+    cap.release()
